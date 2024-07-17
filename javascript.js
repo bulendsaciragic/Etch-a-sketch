5fd3b0c7f02aa83ca7b11789b7410795c3ba1acf
bulendsaciragic <bulendsaciragic@outlook.com>
2024-07-17 09:03:00 +0200

    Add basic CSS

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
-const container = document.querySelector(".container");
+const sketchboard = document.querySelector("#sketchboard");
 
 for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement("div");
-    container.append(squareDiv);
+    squareDiv.setAttribute("id", "squareDiv");
+    sketchboard.append(squareDiv);
 }
 
