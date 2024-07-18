d05c7c42a3fab7a2c7af92d0eef219e21fdbba87
bulendsaciragic <bulendsaciragic@outlook.com>
2024-07-18 18:37:04 +0200

    Create a Grid
    Successfully created a grid using Javascript with some basic CSS styling.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,23 @@
 const sketchboard = document.querySelector("#sketchboard");
 
+let gridSize = 16;
+
+
 for (let i = 0; i < 256; i++) {
+
+    // const squareDivHeight = (sketchboard.offsetHeight - 2) / gridSize;
+    // const squareDivWidth = (sketchboard.offsetHeight - 2) / gridSize;
+
+    const squareDivHeight = sketchboard.clientHeight / gridSize;
+    const squareDivWidth = sketchboard.clientWidth / gridSize;
+    
     const squareDiv = document.createElement("div");
-    squareDiv.setAttribute("id", "squareDiv");
+    squareDiv.setAttribute("class", "squareDiv");
     sketchboard.append(squareDiv);
+    
+    squareDiv.style.height = squareDivHeight + 'px';
+    squareDiv.style.width = squareDivWidth + 'px';
+
 }
+
 
